9b8783b9c7b5b0411f6b0343e844886ada2894bf
Juanfee <juanfelipo8@gmail.com>
2025-05-19 22:51:20 -0500

    fix backend

--- backend/backend/utils/PlanUtils.js
+++
@@ -1,9 +0,0 @@
-const validateUserPlan = (user) => {
-    if (user.plan === 'No tienes un plan' || user.planDuration <= 0) {
-      return { valid: false, message: 'El usuario no tiene un plan activo o el plan ha expirado' };
-    }
-    return { valid: true };
-  };
-  
-  module.exports = { validateUserPlan };
-  
