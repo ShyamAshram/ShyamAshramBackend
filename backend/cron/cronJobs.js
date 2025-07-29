06154d866306be974c801c134d0326345d839f74
Juanfee <juanfelipo8@gmail.com>
2025-07-28 22:13:33 -0500

    TAREAS PROGRAMADAS

--- backend/cron/cronJobs.js
+++ backend/cron/cronJobs.js
@@ -1,15 +1,15 @@
 const cron = require('node-cron');
-const User = require('./../models/user'); // Asegúrate de importar el modelo
-const sendEmail = require('./../utils/sendEmail'); // Función para enviar correos
-const sendWhatsApp = require('./../utils/sendWhatsApp'); // Función para WhatsApp
-const sendPushNotification = require('./../utils/sendPushNotification'); // Función para notificaciones push
+const User = require('./../models/user'); 
+const sendEmail = require('./../utils/sendEmail');
+const sendWhatsApp = require('./../utils/sendWhatsApp'); 
+const sendPushNotification = require('./../utils/sendPushNotification'); 
 
 
-cron.schedule('0 9 * * *', async () => {  // Se ejecuta todos los días a las 9 AM
+cron.schedule('0 9 * * *', async () => {  
   console.log('Ejecutando tarea programada de recordatorio de planes.');
 
   const users = await User.find({
-    planDuration: { $lte: 3, $gt: 0 }, // Usuarios con menos de 3 días de plan restante
+    planDuration: { $lte: 3, $gt: 0 }, 
   });
 
   users.forEach(async (user) => {
