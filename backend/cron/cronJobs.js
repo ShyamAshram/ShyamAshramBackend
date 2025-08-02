6ade3993913ac7005fffc9763c7e3cc1b0be4545
ShyamAshram <shyamashram@gmail.com>
2025-08-02 17:37:04 -0500

    Update cronJobs.js

--- backend/cron/cronJobs.js
+++ backend/cron/cronJobs.js
@@ -1,22 +1,22 @@
-const cron = require('node-cron');
-const User = require('./../models/user'); 
-const sendEmail = require('./../utils/sendEmail');
-const sendWhatsApp = require('./../utils/sendWhatsApp'); 
-const sendPushNotification = require('./../utils/sendPushNotification'); 
+//const cron = require('node-cron');
+//const User = require('./../models/user'); 
+//const sendEmail = require('./../utils/sendEmail');
+//const sendWhatsApp = require('./../utils/sendWhatsApp'); 
+//const sendPushNotification = require('./../utils/sendPushNotification'); 
 
 
-cron.schedule('0 9 * * *', async () => {  
-  console.log('Ejecutando tarea programada de recordatorio de planes.');
+//cron.schedule('0 9 * * *', async () => {  
+  //console.log('Ejecutando tarea programada de recordatorio de planes.');
 
-  const users = await User.find({
-    planDuration: { $lte: 3, $gt: 0 }, 
-  });
+  //const users = await User.find({
+    //planDuration: { $lte: 3, $gt: 0 }, 
+  //});
 
-  users.forEach(async (user) => {
-    const message = `Hola ${user.name}, tu plan "${user.plan}" está por expirar. ¡Renueva ahora para no perder acceso!`;
+  //users.forEach(async (user) => {
+    //const message = `Hola ${user.name}, tu plan "${user.plan}" está por expirar. ¡Renueva ahora para no perder acceso!`;
 
-    await sendEmail(user.email, 'Recordatorio de vencimiento de plan', message);
-    await sendWhatsApp(user.phonenumber, message);
-    await sendPushNotification(user._id, message);
-  });
-});
+    //await sendEmail(user.email, 'Recordatorio de vencimiento de plan', message);
+    //await sendWhatsApp(user.phonenumber, message);
+    //await sendPushNotification(user._id, message);
+  //});
+//});
